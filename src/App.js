d4183aef6d35165f5757d24490540b0d0714989b
Angelina0905 <yudriangell@gmail>
2025-06-11 01:21:02 +0700

    copy langsung dari video

--- src/App.js
+++ src/App.js
@@ -1,58 +1,24 @@
 import React, { Component } from 'react'
-import { Row, Col, Container } from "react-bootstrap";
-import ListCategories from "./components/ListCategories";
-import Hasil from "./components/Hasil";
-import NavbarComponent from "./components/NavbarComponent";
-import { API_URL } from './utils/constants'
-import axios from 'axios';
-
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+} from "react-router-dom";
+import { NavbarComponent } from './components'
+import { Home, Sukses } from './pages'
 
 export default class App extends Component {
-  constructor(props) {
-    super(props)
-  
-    this.state = {
-       menus: [],
-    }
-  }
-  
-  componentDidMount(){
-    axios
-    .get(API_URL+ `products`)
-    .then(res => {
-      const menus  = res.data;
-      this.setState({ menus });
-    })
-    .catch(error => {
-      console.error(error);
-    }
-    )
-  }
   render() {
-    console.log(this.state.menus);
     return (
-      <div>
-        <div className="App">
-      <NavbarComponent />
-      <div className="mt-5">
-        <Container fluid>
-          <Row>
-            <ListCategories />
-            <Col>
-              <h4>
-                <strong>Daftar Produk</strong>
-              </h4>
-              <hr />
-            </Col>
-            <Hasil />
-          </Row>
-        </Container>
-      </div>
-    </div>
-      </div>
+      <BrowserRouter>
+          <NavbarComponent />
+          <main>
+            <Switch>
+              <Route  path="/" component={Home} exact/>
+              <Route  path="/sukses" component={Sukses} exact/>
+            </Switch>
+          </main>
+      </BrowserRouter>
     )
   }
 }
-
-
-
