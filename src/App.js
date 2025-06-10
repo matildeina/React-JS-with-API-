f9d4799215c9eee059a5a5f5be85792e362662be
Angelina0905 <yudriangell@gmail>
2025-06-11 00:48:34 +0700

    belum jadi(bagian 5 video)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { Row, Col, Container } from "react-bootstrap";
+import ListCategories from "./components/ListCategories";
+import Hasil from "./components/Hasil";
+import NavbarComponent from "./components/NavbarComponent";
+import { API_URL } from './utils/constants'
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+export default class App extends Component {
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       menus: [],
+    }
+  }
+  
+  componentDidMount(){
+    axios
+    .get(API_URL+ `products`)
+    .then(res => {
+      const menus  = res.data;
+      this.setState({ menus });
+    })
+    .catch(error => {
+      console.error(error);
+    }
+    )
+  }
+  render() {
+    console.log(this.state.menus);
+    return (
+      <div>
+        <div className="App">
+      <NavbarComponent />
+      <div className="mt-5">
+        <Container fluid>
+          <Row>
+            <ListCategories />
+            <Col>
+              <h4>
+                <strong>Daftar Produk</strong>
+              </h4>
+              <hr />
+            </Col>
+            <Hasil />
+          </Row>
+        </Container>
+      </div>
     </div>
-  );
+      </div>
+    )
+  }
 }
 
-export default App;
+
+
