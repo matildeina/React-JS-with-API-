0d42ac781e1cccb8b6c913739ffea0c9a5c42672
Angelina0905 <yudriangell@gmail>
2025-06-20 13:04:18 +0700

    masih di bagian hasil

--- src/App.js
+++ src/App.js
@@ -1,58 +1,151 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 import { Row, Col, Container } from "react-bootstrap";
 import ListCategories from "./components/ListCategories";
 import Hasil from "./components/Hasil";
 import NavbarComponent from "./components/NavbarComponent";
-import { API_URL } from './utils/constants'
-import axios from 'axios';
-
+import { API_URL } from "./utils/constants";
+import axios from "axios";
+import { Menus } from "./components";
+import swal from "sweetalert";
 
 export default class App extends Component {
   constructor(props) {
-    super(props)
-  
+    super(props);
+
     this.state = {
-       menus: [],
-    }
+      menus: [],
+      categoriYangDipilih: "Cemilan",
+      keranjangs: [],
+    };
   }
-  
-  componentDidMount(){
+
+  componentDidMount() {
     axios
-    .get(API_URL+ `products`)
-    .then(res => {
-      const menus  = res.data;
-      this.setState({ menus });
-    })
-    .catch(error => {
-      console.error(error);
-    }
-    )
+      .get(API_URL + `products?category.nama=` + this.state.categoriYangDipilih)
+      .then((res) => {
+        // console.log("Response: ", res);
+        const menus = res.data;
+        this.setState({ menus });
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+      axios
+      .get(API_URL + "keranjangs")
+      .then((res) => {
+        // console.log("Response: ", res);
+        const keranjangs = res.data;
+        this.setState({ keranjangs });
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  
+  
   }
+
+  changeCategory = (value) => {
+    this.setState({
+      categoriYangDipilih: value,
+      menus: [],
+    });
+    axios
+      .get(API_URL + `products?category.nama=` + value)
+      .then((res) => {
+        // console.log("Response: ", res);
+        const menus = res.data;
+        this.setState({ menus });
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+
+  };
+
+  // ...existing code...
+masukKeranjang = (value) => {
+  axios
+    .get(API_URL + `keranjangs?product.id=${value.id}`)
+    .then((res) => {
+      if (res.data.length === 0) {
+        // Jika belum ada, buat baru
+        const keranjang = {
+          jumlah: 1,
+          total_harga: value.harga,
+          product: value,
+        };
+
+        axios
+          .post(API_URL + `keranjangs`, keranjang)
+          .then(() => {
+            swal({
+              title: "Berhasil Masuk Keranjang!",
+              text: "Berhasil Masuk Keranjang " + keranjang.product.nama,
+              icon: "success",
+              button: false,
+            });
+          });
+      } else {
+        // Jika sudah ada, update jumlah dan total_harga
+        const keranjangLama = res.data[0];
+        const keranjang = {
+          jumlah: keranjangLama.jumlah + 1,
+          total_harga: keranjangLama.total_harga + value.harga,
+          product: value,
+        };
+
+        axios
+          .put(API_URL + `keranjangs/${keranjangLama.id}`, keranjang)
+          .then(() => {
+            swal({
+              title: "Berhasil Masuk Keranjang!",
+              text: "Berhasil Masuk Keranjang " + keranjang.product.nama,
+              icon: "success",
+              button: false,
+            });
+          });
+      }
+    });
+}
+// ...existing code...
+
+
+
   render() {
-    console.log(this.state.menus);
+    const { menus, categoriYangDipilih, keranjangs } = this.state;
     return (
       <div>
         <div className="App">
-      <NavbarComponent />
-      <div className="mt-5">
-        <Container fluid>
-          <Row>
-            <ListCategories />
-            <Col>
-              <h4>
-                <strong>Daftar Produk</strong>
-              </h4>
-              <hr />
-            </Col>
-            <Hasil />
-          </Row>
-        </Container>
+          <NavbarComponent />
+          <div className="mt-3">
+            <Container fluid>
+              <Row>
+                <ListCategories
+                  changeCategory={this.changeCategory}
+                  categoriYangDipilih={categoriYangDipilih}
+                />
+                <Col>
+                  <h4>
+                    <strong>Daftar Produk</strong>
+                  </h4>
+                  <hr />
+                  <Row>
+                    {menus &&
+                      menus.map((menu) => (
+                        <Menus
+                          key={menu.id}
+                          menu={menu}
+                          masukKeranjang={this.masukKeranjang}
+                        />
+                      ))}
+                  </Row>
+                </Col>
+                <Hasil keranjangs={keranjangs}/>
+              </Row>
+            </Container>
+          </div>
+        </div>
       </div>
-    </div>
-      </div>
-    )
+    );
   }
 }
-
-
-
--- src/components/Hasil.js
+++ src/components/Hasil.js
@@ -1,15 +1,27 @@
-import React, { Component } from 'react'
-import { Col } from 'react-bootstrap'
+import React, { Component } from "react";
+import { Col, ListGroup } from "react-bootstrap";
 
 export default class Hasil extends Component {
   render() {
+    const { keranjangs } = this.props;
     return (
       <Col md={3} mt="2">
         <h4>
-            <strong>Hasil</strong>
+          <strong>Hasil</strong>
         </h4>
         <hr />
+        {keranjangs.length !== 0 && (
+        <ListGroup variant="flush">
+          {keranjangs.map((menuKeranjang) =>(
+            <ListGroup.Item>
+              {menuKeranjang.product.nama}
+            </ListGroup.Item>
+          ))}
+          
+         
+        </ListGroup>
+        )}
       </Col>
-    )
+    );
   }
 }
